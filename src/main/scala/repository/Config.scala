9115773471cae963607a68a1067df7544987191b
Elisa Baum <baum@Baums-MacBook-Pro.local>
2017-06-06 16:46:09 +0200

    make personalAccessToken optional

--- src/main/scala/repository/Config.scala
+++ src/main/scala/repository/Config.scala
@@ -8,13 +8,7 @@ object Config {
 
   def apply(): Config = {
     val apiUrl = System.getenv("githubApiUrl")
-
-    val accessToken = (System.getenv("personalAccessToken") match {
-      case token if token.isEmpty => None
-      case token => Some(token)
-    }) map KMSDecryptionUtil.decrypt
-
-
+    val accessToken = Option(System.getenv("personalAccessToken")) map KMSDecryptionUtil.decrypt
     new Config(apiUrl, accessToken)
   }
 
