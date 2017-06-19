8125a03766c1d49e45cbed9ca34c0b6009c897e5
Elisa Baum <baum@Baums-MacBook-Pro.local>
2017-06-19 11:40:30 +0200

    cleanup

--- build.sbt
+++ build.sbt
@@ -13,7 +13,8 @@ lazy val root = (project in file(".")).
       "com.amazonaws" % "aws-lambda-java-core" % "1.1.0",
       "com.amazonaws" % "aws-java-sdk-kms" % "1.11.135",
       "com.typesafe.play" %% "play-json" % "2.6.0-M1",
-      "org.scalaj" %% "scalaj-http" % "2.3.0","org.scalatest" %% "scalatest" % "3.0.1" % Test,
+      "org.scalaj" %% "scalaj-http" % "2.3.0",
+      "org.scalatest" %% "scalatest" % "3.0.1" % Test,
       "org.mockito" % "mockito-all" % "1.10.19" % Test
     )
   )
--- src/main/scala/repository/util/KMSDecryptionUtil.scala
+++ src/main/scala/repository/util/KMSDecryptionUtil.scala
@@ -3,17 +3,21 @@ package repository.util
 import java.nio.ByteBuffer
 import java.nio.charset.Charset
 
-import com.amazonaws.services.kms.{AWSKMS, AWSKMSClientBuilder}
+import com.amazonaws.services.kms.AWSKMSClientBuilder
 import com.amazonaws.services.kms.model.DecryptRequest
 import com.amazonaws.util.Base64
 
 object KMSDecryptionUtil {
 
-  private val kms: AWSKMS = AWSKMSClientBuilder.defaultClient()
+  private lazy val kmsClient = AWSKMSClientBuilder.defaultClient()
 
   def decrypt(encryptedValue: String): String = {
     val encrypted = Base64.decode(encryptedValue)
-    val decryptedResult = kms.decrypt(new DecryptRequest().withCiphertextBlob(ByteBuffer.wrap(encrypted))).getPlaintext
+
+    val decryptedResult = kmsClient
+      .decrypt(new DecryptRequest().withCiphertextBlob(ByteBuffer.wrap(encrypted)))
+      .getPlaintext
+
     new String(decryptedResult.array(), Charset.forName("UTF-8"))
   }
 
